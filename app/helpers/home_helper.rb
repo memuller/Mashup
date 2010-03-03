597ff495afdf801c54f7597653cd844e7a0cd830
Marco Moura <marco.moura@gmail.com>
2010-03-03 16:36:31 -0300

    ajust layout css

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -4,6 +4,8 @@ module HomeHelper
 			["ga:pageTitle=", "Mashup", "Canção Nova", "cobertura online dos eventos da", "—", "Online", "multimídia para a cobertura online dos eventos"].each do |text|
 				tag = tag.gsub(text,nil.to_s)	
 			end	
-			tag.gsub("+"," ").strip 
+			tag = tag.gsub("+"," ").strip
+			tag = "" if tag.size < 3 unless tag.empty?
+			tag 
 		end
 end

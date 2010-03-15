1c727f8b787aab0f50bd5547ed9a299f31b9e109
Marco Moura <marco.moura@gmail.com>
2010-03-15 15:05:17 -0300

    fix bug locale tag nil

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ ActionController::Routing::Routes.draw do |map|
 	map.about 			'/about',	:controller => "tag", :action => 'about'
 	map.api 				'/api', 	:controller => "tag", :action => 'api'
 
-  map.language '/:locale', :controller => "home", :tag => nil, :requirements => {:locale => /es|en|pt-BR/} 
+  map.language '/:locale', :controller => "home", :requirements => {:locale => /es|en|pt-BR/} 
   map.language '/:locale/:tag', :controller => "tag", :requirements => {:locale => /es|en|pt-BR/}
 
 	map.media '/:action/:tag.:format' , :tag => nil, 	:controller => "tag", :requirements => {:action => /blog|news|video|photo|microblog|bookmark|timeline|cooliris/}

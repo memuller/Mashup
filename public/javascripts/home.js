67c23f6862a597b301245f9edda5259a06e67f74
Marco Moura <marco.moura@gmail.com>
2010-03-23 11:03:30 -0300

    fix js home spotlight

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,5 +1,18 @@
 window.addEvent('domready', function(){
 
+	var nS4 = new noobSlide({
+		box: $('spotlight'),
+		items: $$('#spotlight div'),
+		size: 480,
+		handles: $$('#nav a'),
+		autoPlay: true,
+		onWalk: function(currentItem,currentHandle){
+			this.handles.removeClass('active');
+			currentHandle.addClass('active');
+			currentHandle.addEvent('click',function(event){event.stop();});	
+		}
+	});
+
 	$('search').addEvent('submit', function(event){
 		tag = $('tag').value.clean().replace(" ", "+", "gi");
 		document.location='/'+tag;
@@ -13,15 +26,3 @@ window.addEvent('domready', function(){
 })
 
 
-var nS4 = new noobSlide({
-	box: $('spotlight'),
-	items: $$('#spotlight div'),
-	size: 480,
-	handles: $$('#nav a'),
-	autoPlay: true,
-	onWalk: function(currentItem,currentHandle){
-		this.handles.removeClass('active');
-		currentHandle.addClass('active');
-		currentHandle.addEvent('click',function(event){event.stop();});	
-	}
-});

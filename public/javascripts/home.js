4dbf9a813a9d8041b9132bdcb507fd946eb54564
Marco Moura <marco.moura@gmail.com>
2010-03-23 10:46:12 -0300

    move js spotlight to home.js

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -11,3 +11,17 @@ window.addEvent('domready', function(){
 	})
 	
 })
+
+
+var nS4 = new noobSlide({
+	box: $('spotlight'),
+	items: $$('#spotlight div'),
+	size: 480,
+	handles: $$('#nav a'),
+	autoPlay: true,
+	onWalk: function(currentItem,currentHandle){
+		this.handles.removeClass('active');
+		currentHandle.addClass('active');
+		currentHandle.addEvent('click',function(event){event.stop();});	
+	}
+});

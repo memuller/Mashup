b75d5f3670853f7aa206f063e25966f425ba0d6a
Marco Moura <marco.moura@gmail.com>
2010-03-26 16:40:09 -0300

    fix route mrss

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ ActionController::Routing::Routes.draw do |map|
   map.language '/:locale/:tag', :controller => "tag", :requirements => {:locale => /es|en/}
 
 	map.media '/:action/:tag.:format' , :tag => nil, 	:controller => "tag", :requirements => {:action => /blog|news|video|photo|microblog|bookmark|timeline|cooliris/}
-	map.tag '/:tag.:format' , :controller => "tag", :requirements => { :tag => /.+(?=\.(html|xhtml|rss))|.+/ } 
+	map.tag '/:tag.:format' , :controller => "tag", :requirements => { :tag => /.+(?=\.(html|xhtml|rss|mrss))|.+/ } 
 
-	map.media_tag '/:action/tag/:tag.:format' , :tag => nil, 	:controller => "tag", :requirements => {:action => /blog|news|video|photo|microblog|bookmark|timeline|cooliris/,  :tag => /.+(?=\.(html|xhtml|rss))|.+/ }
+	map.media_tag '/:action/tag/:tag.:format' , :tag => nil, 	:controller => "tag", :requirements => {:action => /blog|news|video|photo|microblog|bookmark|timeline|cooliris/,  :tag => /.+(?=\.(html|xhtml|rss|mrss))|.+/ }
 
   map.search '/:controller/:action.:format', :controller => "home", :tag => nil, :requirements => {:action => /search/} 
 

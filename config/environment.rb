0c1310a4e798bd2be0cc72e40664a05801aaa23b
Matheus E. Muller <hello@memuller.com>
2011-06-23 19:32:28 -0300

    Adds bundler support to legacy Rails.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -20,14 +20,6 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  config.gem "pauldix-feedzirra", :lib => "feedzirra", :source => "http://gems.github.com"
-  config.gem "cannikin-gattica", :lib => "gattica", :source => "http://gems.github.com"
-	config.gem "hash_extension", :source => "http://gemcutter.org"
-	config.gem "diacritics_fu", :source => "http://gemcutter.org"
-	config.gem "rufus-scheduler", :source => "http://gemcutter.org"
-	config.gem 'hoptoad_notifier'
-	config.gem 'metric_fu', :lib => 'metric_fu'
-	
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
